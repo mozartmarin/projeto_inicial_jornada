0db4e959a3338702823e4e1331899f6a8ef5c194
mozartmarin <mozart.marin@gmail.com>
2024-07-14 17:13:05 -0300

    meu primeiro commit

---
+++ main.py
@@ -0,0 +1,19 @@
+print("Jornada de Dados")
+
+#Variaveis
+idade = 50
+nome = "Marin"
+
+#Print
+print(idade)
+
+#Soma
+nova_idade = idade + 1
+print(nova_idade)
+
+#Lista
+lista = [1,2,3,4,5]
+
+#Métodos
+lista_invertida = lista.reverse()
+
